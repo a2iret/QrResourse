85ea1c6fd19b59143f116c25d64963d5c8e4108d
Kanybek <zhanybek20065732@gmail.com>
2025-10-28 00:59:50 +0600

    feature: implemented endpoint for generating qr code link to the https://www.google.com

--- src/main/java/com/example/QrResource/controller/QrController.java
+++ src/main/java/com/example/QrResource/controller/QrController.java
@@ -1,15 +1,26 @@
 package com.example.QrResource.controller;
 
+import com.example.QrResource.service.QrCodeGenerator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("api/v1")
 public class QrController {
+    private final QrCodeGenerator qrCodeGenerator;
 
-    @GetMapping("generate/qr")
-    public String generateQr(){
-        return "";
+    @PostMapping("generate/qr")
+    public ResponseEntity<byte[]> generateQr(){
+        byte[] qrCode = qrCodeGenerator.generateQrCode("https://www.google.com");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+        return ResponseEntity.ok().headers(headers).body(qrCode);
     }
 }
---
+++ src/main/java/com/example/QrResource/model/QrCode.java
@@ -0,0 +1,16 @@
+package com.example.QrResource.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class QrCode {
+    private Integer size;
+    private QrCodeType type;
+    private String data;
+
+    private enum QrCodeType {
+        STATIC, DYNAMIC
+    }
+}
