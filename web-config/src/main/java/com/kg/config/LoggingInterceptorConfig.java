08979ecb5aa20c82de0b67d5666fd08f6581347c
Aziret java <jut.su312@gmail.com>
2025-11-03 23:20:47 +0600

    Added simple interceptor

---
+++ web-config/src/main/java/com/kg/config/LoggingInterceptorConfig.java
@@ -0,0 +1,19 @@
+package com.kg.config;
+
+import com.kg.intercaptor.LoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class LoggingInterceptorConfig implements WebMvcConfigurer {
+    private final LoggingInterceptor loggingInterceptorConfig;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptorConfig).addPathPatterns("api/");
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+}
